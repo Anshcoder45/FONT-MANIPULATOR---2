8db29f3db2bb8cbe0eeb16354f5cdf214e9fbe7a
Anshcoder45 <93387770+Anshcoder45@users.noreply.github.com>
2023-07-13 10:58:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+}
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(550, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+if (results.length > 0)
+{
+    console.log(results);
+}
+}
+function modelLoaded(){
+    console.log('PoseNet is initialized');
+}
+function draw(){
+    background("darksalmon");
+}
